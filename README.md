224d3cd17c369b54621e509ba396ac30416c91d5
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-10-04 20:55:34 -0400

    Initial setup, added README.
    Imported linked-list code from previous project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# odin-hashmap
+This will follow the Odin Project's HashMap project. Using the linked list code I created in the previous project, I will create a hash map implementation. These hash maps will have all the expected methods.
+
+This project will demonstrate my understanding of hash maps and ability to create and utilize important data structures.
---
+++ linked-list.js
@@ -0,0 +1,94 @@
+class LinkedList {
+  constructor(list) {
+    let linkedList = { value: list[0] };
+    this.head = linkedList;
+    let item = linkedList;
+    let total = 1;
+    this.tail = item;
+    for (let i = 1; i < list.length; i++) {
+      this.append(list[i]);
+      total++;
+    }
+    this.size = total;
+  }
+  Node(value) {
+    return { value, next: null, previous: null };
+  }
+  append(value) {
+    const item = this.Node(value);
+    this.tail.next = item;
+    item.previous = this.tail;
+    this.tail = item;
+    item.next = null;
+  }
+  prepend(value) {
+    const item = this.Node(value);
+    item.next = this.head;
+    item.previous = null;
+    this.head = item;
+  }
+  at(index) {
+    let item = this.head;
+    for (let i = 0; i < index; i++) {
+      item = item.next;
+    }
+    return item;
+  }
+  pop() {
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+  }
+  toString() {
+    let item = this.head;
+    let string = "";
+    while (item.next !== null) {
+      string += `(${item.value})->`;
+      item = item.next;
+    }
+    string += `(${item.value})`;
+    return string;
+  }
+  contains(value) {
+    let item = this.head;
+    do {
+      if (item.value === value) {
+        return true;
+      }
+      item = item.next;
+    } while (item.next !== null);
+    return false;
+  }
+  find(value) {
+    let item = this.head;
+    let i = 0;
+    do {
+      item = item.next;
+      i++;
+      if (item.value === value) {
+        return i;
+      } else {
+      }
+    } while (item.next !== null);
+    return null;
+  }
+  insertAt(value, index) {
+    let item = this.head;
+    const insert = this.Node(value);
+    for (let i = 0; i < index - 1; i++) {
+      item = item.next;
+    }
+    insert.next = item.next;
+    insert.previous = item;
+    item.next = insert;
+    insert.next.previous = insert;
+  }
+  removeAt(index) {
+    let item = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      item = item.next;
+    }
+    item.next = item.next.next;
+  }
+}
+
+export default LinkedList;
